0af7a13fd1ea7f479300b33ed96476819759323a
m171kumar <68642597+m171kumar@users.noreply.github.com>
2024-04-23 22:05:22 +0100

    Create new.tf

---
+++ new.tf
@@ -0,0 +1,77 @@
+terraform {
+  required_providers {
+    databricks = {
+      source = "databricks/databricks"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.15.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
+
+// initialize provider in "MWS" mode to provision new workspace
+provider "databricks" {
+  alias         = "mws"
+  host          = "https://accounts.cloud.databricks.com"
+  account_id    = var.databricks_account_id
+  client_id     = var.client_id
+  client_secret = var.client_secret
+}
+
+---------
+data "databricks_aws_assume_role_policy" "this" {
+  provider    = databricks.mws
+  external_id = var.databricks_account_id
+}
+
+resource "aws_iam_role" "cross_account_role" {
+  name               = "${local.prefix}-crossaccount"
+  assume_role_policy = data.databricks_aws_assume_role_policy.this.json
+  tags               = var.tags
+}
+
+data "databricks_aws_crossaccount_policy" "this" {
+  provider    = databricks.mws
+  policy_type = "customer"
+}
+
+resource "aws_iam_role_policy" "this" {
+  name   = "${local.prefix}-policy"
+  role   = aws_iam_role.cross_account_role.id
+  policy = data.databricks_aws_crossaccount_policy.this.json
+}
+
+resource "databricks_mws_credentials" "this" {
+  provider         = databricks.mws
+  account_id       = var.databricks_account_id
+  role_arn         = aws_iam_role.cross_account_role.arn
+  credentials_name = "${local.prefix}-creds"
+  depends_on       = [aws_iam_role_policy.this]
+}
+--------
+
+
+
+data "databricks_node_type" "smallest" {
+  local_disk = true
+}
+
+data "databricks_spark_version" "latest_lts" {
+  long_term_support = true
+}
+
+resource "databricks_cluster" "shared_autoscaling" {
+  cluster_name            = "Shared Autoscaling"
+  spark_version           = data.databricks_spark_version.latest_lts.id
+  node_type_id            = data.databricks_node_type.smallest.id
+  autotermination_minutes = 20
+  autoscale {
+    min_workers = 1
+    max_workers = 50
+  }
+}
